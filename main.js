885de6052c21505445f5a4e018432cd201646c60
Srushtika <n.srushtika@gmail.com>
2018-08-31 12:05:08 +0530

    final cleanup

--- main.js
+++ main.js
@@ -10,24 +10,23 @@ var msgObjArray = [];
 
 //----------------------------AblyStuff
 var myId = "id-" + Math.random().toString(36).substr(2, 16)
-console.log("My ID is " + myId)
 var apiKey = '<ABLY-API-KEY>'
 var ably = new Ably.Realtime({
     key: apiKey,
     clientId: myId,
     echoMessages: false
 })
-console.log("My avatar is " + myAvatar)
 var chatChannel = ably.channels.get("chat")
 var readReceiptsChannel = ably.channels.get("read-receipts")
 var presenceChannel = ably.channels.get("presence")
+//----------------------------
+
 
 chatChannel.subscribe("userAvatar", (data) => {
     var dataObj = JSON.parse(JSON.stringify(data))
     if (dataObj.clientId != myId) {
         otherAvatar = dataObj.data.avatar
         you.avatar = otherAvatar
-        console.log('Other users\'s avatar is ' + otherAvatar)
     }
 })
 
@@ -64,10 +63,7 @@ function insertChat(who, text, currentMsgId) {
     var control = "";
     var date = formatAMPM(new Date());
     var seenStatus = false
-
-    console.log('Inserting Id' + currentMsgId)
     if (who == "me") {
-        console.log('Entered me')
         control = '<li class="self">' +
             '<div style="color: #e0e0de" class="avatar">' +
             '<img src="' + me.avatar + '" />' +
@@ -78,7 +74,6 @@ function insertChat(who, text, currentMsgId) {
             '</div>' +
             '</li>';
     } else {
-        console.log('Entered you')
         control = '<li class="other">' +
             '<div style="color: #e0e0de" class="avatar">' +
             '<img src="' + you.avatar + '" />' +
@@ -109,7 +104,6 @@ function insertChat(who, text, currentMsgId) {
 }
 
 window.onfocus = function () {
-    console.log('Window now focused')
     readReceiptsChannel.publish("receipt", {
         "singleMsgSeen": false,
         "seen": true
@@ -117,34 +111,41 @@ window.onfocus = function () {
 
 };
 
-
 function resetChat() {
     $("ul").empty();
 }
 
+window.onload = inputKeyUpEvent;
+
+function inputKeyUpEvent(){
+    var input = document.getElementById("myMsg");
+    input.addEventListener("keyup", function(event) {
+      event.preventDefault();
+      if (event.keyCode === 13) {
+        document.getElementById("sendMessage").click();
+      }
+    });
+}
+
+
 function sendMyMessage() {
     var currentMsgId = "msg-id-" + Math.random().toString(36).substr(2, 6)
-    console.log('Send clicked by ' + myId)
-    console.log(document.getElementById("myMsg"))
     var text = document.getElementById("myMsg").value
-    console.log('Text is ' + text)
+    document.getElementById("myMsg").value = ""
     if (text !== "") {
         insertChat("me", text, currentMsgId);
         chatChannel.publish("chatMessage", {
             message: text,
             msgId: currentMsgId
         })
-        console.log('Published')
         $(this).val('');
     }
 }
 
 readReceiptsChannel.subscribe("receipt", (data) => {
-    console.log('Receipt Received')
     var dataObj = JSON.parse(JSON.stringify(data))
     if (dataObj.data.singleMsgSeen) {
         var receiptMsgId = dataObj.data.msgId
-        console.log(receiptMsgId)
         if (dataObj.data.seen) {
             if (document.getElementById(receiptMsgId)) {
                 (document.getElementById(receiptMsgId)).innerHTML = 'seen'
@@ -157,7 +158,6 @@ readReceiptsChannel.subscribe("receipt", (data) => {
     } else if (!dataObj.data.singleMsgSeen) {
         for (var i = 0; i < msgObjArray.length; i++) {
             var id = msgObjArray[i].msgId
-            console.log('Inside on focus ids are' + id)
             if (document.getElementById(id)) {
                 (document.getElementById(id)).innerHTML = 'seen'
             }
@@ -171,9 +171,7 @@ resetChat();
 
 //-- Print Messages
 chatChannel.subscribe("chatMessage", (data) => {
-    console.log('Received')
     var dataObj = JSON.parse(JSON.stringify(data))
-    console.log(dataObj)
     var message = dataObj.data.message
     var myMsgId = dataObj.data.msgId
     insertChat("you", message, myMsgId);
